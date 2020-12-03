a4a65a085c2736187ee261b22746e5aa6de92484
Rafiqul Islam <rafiqul.islam@s2018.tu-chemnitz.de>
2020-12-03 12:20:00 +0100

    Obstacle avoidance first version

--- obstacle_avoidance.cpp
+++ obstacle_avoidance.cpp
@@ -105,7 +105,7 @@ int main(int argc, char** argv)
         Mat org_grey;
         cvtColor(image, org_grey, CV_BGR2GRAY); //Convert to greyscale
         //org_grey.convertTo(org_grey, CV_8U, 255.0);
-        imshow("Original image ", org_grey);
+        imshow("Input image ",image);
         float THRESHOLD = 100; //0=black/obstacle 255=white/no obstacle
         int counter; //number of pixel inside the grid that are greater than the threshold
 
@@ -114,7 +114,7 @@ int main(int argc, char** argv)
             counter = 0;
             cvtColor(cropped, grey, CV_BGR2GRAY, 255.0);
             grey.convertTo(grey, CV_8UC1); // convert pixel value between 0 and 255
-            imshow("Converted greyscale", grey);
+            //imshow("Converted greyscale", grey);
             float max = -999999.0; //maximum pixel value inside the grid
             float min = 999999.0; // minimum pixel value inside the grid
             for (int i = 0; i < cropped.rows; i++) {
@@ -133,17 +133,22 @@ int main(int argc, char** argv)
             cout << dir[direction] << " # of pixels greater than threshold " << counter << " Max " << max << " Min " << min << endl;
             coordinates.push_back(Coordinates(grid[predefined_direction[direction]].x, grid[predefined_direction[direction]].y, GRID_SIZE_WIDTH, GRID_SIZE_HEIGHT, dir[direction], counter));
             //coordinates[direction].display();
-            imshow("Grayscale", grey);
+            //imshow("Grayscale", grey);
 
             //imshow(dir[direction],cropped);
-            waitKey();
+            //waitKey();
         }
 
         //Test
         sort(coordinates.begin(),coordinates.end());
-        for(int i=0;i<coordinates.size();i++){
+        /*for(int i=0;i<coordinates.size();i++){
             coordinates[i].display();
-        }
+        }*/
+        Rect r=Rect(coordinates[coordinates.size()-1].x,coordinates[coordinates.size()-1].y,coordinates[coordinates.size()-1].w,coordinates[coordinates.size()-1].h); 
+        rectangle(image,r,Scalar(0,0,255),2);
+        imshow("Obstacle free grid",image);
+        waitKey();
+        
     }
     else {
         cout << "Wrong command" << endl;
