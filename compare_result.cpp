03f4eea5b4d5e9cda6ec1255778a99857332250a
Rafiqul Islam <rafiqul.islam@s2018.tu-chemnitz.de>
2021-01-24 08:25:32 +0100

    nothing changed

--- compare_result.cpp
+++ compare_result.cpp
@@ -17,15 +17,15 @@ using namespace std;
 int main() {
     string folder_1,folder_2;
     vector<cv::String>images_1,images_2;
-    folder_1="/home/rafiqul/Documents/Thesis/Code/GitHub/masters_thesis/Obstacle-Avoidance-from-Image/exp1";
-    folder_2="/home/rafiqul/Documents/Thesis/Code/GitHub/masters_thesis/Obstacle-Avoidance-from-Image/exp2";
+    folder_1="/home/rafiqul/exp1";
+    folder_2="/home/rafiqul/exp2";
     cv::glob(folder_1, images_1, false);
     cv::glob(folder_2, images_2, false);
     Mat img1,img2;
     for(int i=0;i<images_1.size();i++) {
         img1=imread(images_1[i]);
         img2=imread(images_2[i]);
-        hconcat(img1,img2,img1);
+        vconcat(img1,img2,img1);
         imshow("Compare  ",img1);
         waitKey();
     }
